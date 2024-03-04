99774702c66f9bbdf9100f0149ca4adee51be6cb
Natalie Lockyer <Natalie_lockyer@outlook.com>
2024-03-04 19:41:21 +0000

    Database_url added

--- plantlife/settings.py
+++ plantlife/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2t+%8+*oc2*3mvhab*f(vm@bcy=iq48d-lixpmhzwuzo-^zad='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-natalielock-plantlifebl-dfp08rw1gxw.ws-eu108.gitpod.io', '.herokuapp.com']
 
