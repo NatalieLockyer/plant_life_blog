b657e17e3190054f145b3a686267a376d0413777
Natalie Lockyer <Natalie_lockyer@outlook.com>
2024-03-07 16:18:38 +0000

    Installed Cloudinary

--- recipes/models.py
+++ recipes/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
 class Post(models.Model):
     """
-    Stores a single blog/comment post entry related to :model:`auth.User`.
+    Stores a single blog post entry related to :model:`auth.User`.
     """
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="recipe_posts")
-    content = models.TextField()
+    image = CloudinaryField('image', default='placeholder')
+    details = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
