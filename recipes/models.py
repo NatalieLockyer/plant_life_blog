ff0eed09e8fb22d284e4728c9188d961307a8768
Natalie Lockyer <Natalie_lockyer@outlook.com>
2024-03-07 16:59:41 +0000

    Added duration and serves to POST model

--- recipes/models.py
+++ recipes/models.py
@@ -2,7 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
+import datetime
+
 STATUS = ((0, "Draft"), (1, "Published"))
+PEOPLE = ((0, "Serves 1"), (1, "Serves 2"), (2, "Serves 2 - 4"))
 
 class Post(models.Model):
     """
@@ -13,6 +16,8 @@ class Post(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="recipe_posts")
     image = CloudinaryField('image', default='placeholder')
+    duration = models.TextField(blank=True)
+    serves = models.IntegerField(choices=PEOPLE, default=0)
     details = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
