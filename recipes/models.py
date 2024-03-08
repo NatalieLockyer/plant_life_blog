84acecb566580580bd68b8bcb962ce332c71fa0c
Natalie Lockyer <Natalie_lockyer@outlook.com>
2024-03-08 11:16:33 +0000

    Amendments to the Post Models arguements

--- recipes/models.py
+++ recipes/models.py
@@ -11,19 +11,19 @@ class Post(models.Model):
     """
     Stores a single blog post entry related to :model:`auth.User`.
     """
-    title = models.CharField(max_length=200, unique=True)
+    title = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="recipe_posts")
     image = CloudinaryField('image', default='placeholder')
-    duration = models.TextField(blank=True)
+    duration = models.TextField(max_length=20, default=False)
     serves = models.IntegerField(choices=PEOPLE, default=0)
-    source_of = models.TextField(blank=True)
+    source_of = models.TextField(max_length=50, default=False)
     details = models.TextField()
-    ingredients = models.TextField(blank=True)
-    method = models.TextField(blank=True)
+    ingredients = models.TextField(max_length=300, default=False)
+    method = models.TextField(max_length=500, default=False)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    excerpt = models.TextField(blank=True)
+    excerpt = models.TextField(max_length=200, default=False)
     updated_on = models.DateTimeField(auto_now=True)
     
