6e5d19f662c9cd6b49f24bddcbd4ddf18e318eb5
Natalie Lockyer <Natalie_lockyer@outlook.com>
2024-03-10 15:54:26 +0000

    Added a post admin class

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    """
+    """
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
 """
 This will allow me to create, update 
 & delete blog posts from the admin panel
 """
-admin.site.register(Post)
 admin.site.register(Comment)
