049b92e74696aa6cf423c342aa75c9f44896c160
Natalie Lockyer <Natalie_lockyer@outlook.com>
2024-03-16 14:55:07 +0000

    added post_detail view

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
@@ -7,6 +7,30 @@ class PostList(generic.ListView):
     template_name = "recipes/index.html"
     paginate_by = 6
  
+
+def post_detail(request, slug):
+    """
+    Display an individual :model:`recipes.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`recipes.Post`.
+
+    **Template**
+
+    :template:`recipe/post_detail.html`
+    
+    """
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug-slug)
+
+    return render (
+        request,
+        "recipes/post_detail.html",
+        {"post": post},
+    )
 
 
 
