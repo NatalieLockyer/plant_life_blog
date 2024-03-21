50592a67235ed9ab65e18b6a8a4913a595d17de7
Natalie Lockyer <Natalie_lockyer@outlook.com>
2024-03-21 10:54:34 +0000

    Changed login/logout URL

--- plantlife/settings.py
+++ plantlife/settings.py
@@ -57,8 +57,8 @@ INSTALLED_APPS = [
 ]
 
 SITE_ID = 1
-LOGIN_REDIRECT_URL = 'https://8000-natalielock-plantlifebl-dfp08rw1gxw.ws-eu110.gitpod.io'
-LOGOUT_REDIRECT_URL = 'https://8000-natalielock-plantlifebl-dfp08rw1gxw.ws-eu110.gitpod.io'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
